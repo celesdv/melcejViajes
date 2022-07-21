49cf591c2119a0769a95d51ea382bc454f5b3133
celesdv <66212987+celesdv@users.noreply.github.com>
2022-07-21 16:34:27 -0300

    :tada: add new proyect

---
+++ home/src/test/java/com/melcej/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melcej.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
